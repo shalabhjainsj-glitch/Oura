234ce196923296c8636df10d2be910ad0ee39685
shalabhjainsj-glitch <shalabh.jain.sj@gmail.com>
2026-07-24 12:11:30 +0530

    Modify Delivery & Billing Information markdown
    Updated markdown for Delivery & Billing Information section.

--- Oura_app.py
+++ Oura_app.py
@@ -1624,7 +1624,7 @@ if st.session_state.cart:
                     st.success(f"**{name} UPI ID:** `{data['id']}`")
 
     st.markdown("---")
-    st.markdown(f"### 📍 {t('Delivery & Billing Information', 'डिलीवरी और बिल की जानकारी')}")
+    st.markdown(f"### 📍 
     
     with st.form("billing_form"):
         col_d1, col_d2 = st.columns(2)
